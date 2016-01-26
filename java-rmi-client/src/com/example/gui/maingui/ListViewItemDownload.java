031a7765368b7b1e89433facecc0403d31bfabeb
Brolly <tomhanusiak@gmail.com>
2016-01-26 22:29:25 +0100

    [FLU-15] - creating dowload list

--- java-rmi-client/src/com/example/gui/maingui/ListViewItemDownload.java
+++ java-rmi-client/src/com/example/gui/maingui/ListViewItemDownload.java
@@ -1,7 +1,82 @@
 package com.example.gui.maingui;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.stage.FileChooser;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+
 /**
  * Created by Brolly on 13.01.2016.
  */
-public class ListViewItemDownload {
+public class ListViewItemDownload extends HBox {
+    private Button dowloadButtonItem;
+    private File file;
+    private Label label;
+
+    static ArrayList<File> listFileToDowload = new ArrayList<>();
+
+    public ListViewItemDownload(File file) {
+        super();
+        this.file = file;
+        listFileToDowload.add(file);
+        Image imageFile = new Image("file:java-rmi-client/media/text-x-generic.png");
+        label = new Label(file.getAbsolutePath());
+        label.setGraphic(new ImageView(imageFile));
+        label.getStyleClass().add("blacklabel");
+        label.setMaxWidth(Double.MAX_VALUE);
+        HBox.setHgrow(label, Priority.ALWAYS);
+        dowloadButtonItem = new Button("Dowload");
+        dowloadButtonItem.setOnAction(event1 -> {
+            saveFileChooser();
+            findFileToRemove(file);
+            Image imageDowload = new Image("file:java-rmi-client/media/downloaded.png");
+            dowloadButtonItem.setGraphic(new ImageView(imageDowload));
+            dowloadButtonItem.setText("");
+            dowloadButtonItem.setDisable(true);
+            label.getStyleClass().remove("blacklabel");
+
+        });
+        this.getChildren().addAll(label, dowloadButtonItem);
+    }
+
+
+    private void findFileToRemove(File file) {
+        ArrayList<File> list = listFileToDowload;
+        Iterator<File> it = list.iterator();
+        while (it.hasNext()) {
+            File temp = it.next();
+            if (temp == file)
+                it.remove();
+        }
+    }
+
+    private void SaveFile(String content, File file) {
+        try {
+            FileWriter fileWriter = null;
+
+            fileWriter = new FileWriter(file);
+            fileWriter.write(content);
+            fileWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void saveFileChooser()
+    {
+        FileChooser fileChooser = new FileChooser();
+        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("TXT files (*.txt)", "*.txt");
+        fileChooser.getExtensionFilters().add(extFilter);
+        File file = fileChooser.showSaveDialog(MainGUI.stage);
+    }
 }
